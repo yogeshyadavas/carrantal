7f231794414750856e72f7cf0f0c8a2c2b594e78
Yogesh Yadav <75256665+yogeshyadavas@users.noreply.github.com>
2022-03-18 06:39:19 +0530

    db name change

--- includes/config.php
+++ includes/config.php
@@ -3,7 +3,7 @@
 define('DB_HOST','40.71.8.203');
 define('DB_USER','yogesh@testcarpro');
 define('DB_PASS','Yogi#4232');
-define('DB_NAME','testcarpro');
+define('DB_NAME','carrental');
 // Establish database connection.
 try
 {
